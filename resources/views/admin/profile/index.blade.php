6f1814361e2ba355120340bd26c007c68756f3b5
vinnie <mymailaddress010020@gmail.com>
2023-03-26 01:01:09 +0630

    change ui with templete done

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -1,34 +1,67 @@
 @extends('admin.layouts.app')
 
 @section('content')
-    <form>
-        <div class="row">
-            <div class="col mb-2">
-                <label for="exampleInputName" class="form-label">Name</label>
-                <input type="text" class="form-control" id="exampleInputName" placeholder="Enter Name">
+    <div class="col-8 offset-3 mt-5">
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header p-2">
+                    <legend class="text-center">User Profile</legend>
+                </div>
+                <div class="card-body">
+                    <div class="tab-content">
+                        <div class="active tab-pane" id="activity">
+                            <form class="form-horizontal">
+                                <div class="form-group row">
+                                    <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="inputName" placeholder="Enter Name">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="inputEmail"
+                                            placeholder="Enter Email">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputEmail" class="col-sm-2 col-form-label">Phone</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="inputEmail"
+                                            placeholder="Enter Phone">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputEmail" class="col-sm-2 col-form-label">Address</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="inputEmail"
+                                            placeholder="Enter Address">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputEmail" class="col-sm-2 col-form-label">Gender</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="inputEmail"
+                                            placeholder="Enter Gender">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="offset-sm-2 col-sm-10">
+                                        <a href="">Change Password</a>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="offset-sm-2 col-sm-10">
+                                        <button type="submit" class="btn bg-dark text-white">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col mb-2">
-                <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Email">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col mb-2">
-                <label for="exampleInputPhone" class="form-label">Phone</label>
-                <input type="number" class="form-control" id="exampleInputPhone" placeholder="Enter Phone">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col mb-2">
-                <label for="exampleInputAddress" class="form-label">Address</label>
-                <textarea cols="30" rows="10" class="form-control" id="exampleInputAddress" placeholder="Enter Address"></textarea>
-            </div>
-        </div>
-        <div class="text-center">
-            <button type="submit" class="btn btn-dark w-50">Update</button>
-        </div>
-    </form>
-    <a href="" class="d-block text-end mt-3">change password</a>
+    </div>
 @endsection
