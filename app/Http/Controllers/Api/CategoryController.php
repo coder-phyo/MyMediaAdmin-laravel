b3535ef6fa2248f3f6b92fd285795794d10d0aec
vinnie <mymailaddress010020@gmail.com>
2023-04-06 00:56:48 +0630

    api route for search post done

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,16 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     // get all category list
     public function getAllCategory()
     {
-        $category = Category::get();
+        $category = Category::select('category_id', 'title', 'description')->get();
 
         return response()->json([
             'category' => $category,
+        ]);
+    }
+
+    // search category
+    public function postSearch(Request $request)
+    {
+        $post = Post::where('title', 'like', '%' . $request->key . '%')->get();
+        return response()->json([
+            'searchValue' => $post,
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,5 +13,7 @@ Route::post('user/register', [AuthController::class, 'register']);
 
 // post
 Route::get('allPostList', [PostController::class, 'getAllPost']);
+
 // category
 Route::get('allCategory', [CategoryController::class, 'getAllCategory']);
+Route::post('post/search', [CategoryController::class, 'postSearch']);
